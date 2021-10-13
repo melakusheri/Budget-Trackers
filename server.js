81246f67394c4a15c7e138c351e6106209f283b1
Melaku Sheri <mailangels@gmail.com>
2021-10-13 12:45:33 -0400

    mongodb connect

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const compression = require("compression");
 
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3000
 
-app.use(logger("dev"));
-
-app.use(compression());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+app.use(compression());
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
